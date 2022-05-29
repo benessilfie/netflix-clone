2e0bf7eda4b48da94f062e481f2565a7f62ddd8c
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2022-05-29 13:34:20 +0000

    Finish Banner build

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { baseUrl } from '../constants/movie'
 import { Movie } from '../typings'
+import { FaPlay } from 'react-icons/fa'
+import { InformationCircleIcon } from '@heroicons/react/solid'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -17,7 +19,7 @@ function Banner({ netflixOriginals }: Props) {
   }, [netflixOriginals])
 
   return (
-    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh]  lg:justify-end lg:pb-12">
+    <div className="flex flex-col space-y-2 py-16  md:space-y-4 lg:h-[80vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
@@ -29,9 +31,19 @@ function Banner({ netflixOriginals }: Props) {
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
         {movie?.overview}
       </p>
+
+      <div className="flex space-x-3">
+        <button className="bannerButton bg-white text-black">
+          <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7 " /> Play
+        </button>
+        <button className="bannerButton bg-[gray]/70">
+          More Info
+          <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+        </button>
+      </div>
     </div>
   )
 }
